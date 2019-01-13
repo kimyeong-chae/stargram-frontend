bc4606854090647e7a73e56b2893b2c7caf9d4dd
kimyeong-chae <rdc3884@gmail.com>
2019-01-13 12:22:54 +0900

    1. 비밀번호 찾기, 회원가입 페이지 퍼블리싱 완료 커밋

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login', { });
+});
+
+/* GET home page. */
+router.get('/password', function(req, res, next) {
+    res.render('password', { });
+});
+
+/* GET home page. */
+router.get('/register', function(req, res, next) {
+    res.render('register', { });
 });
 
 module.exports = router;
