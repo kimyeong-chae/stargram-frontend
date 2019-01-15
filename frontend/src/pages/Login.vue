769d1dbbf6afac7549762bbf45fc5cc61fcaa1c3
mkki <gosorn1@gmail.com>
2019-01-15 22:56:00 +0900

    Add login & registration pages

---
+++ frontend/src/pages/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    <login-form/>
+    <find-password/>
+  </div>
+</template>
+
+<script>
+import LoginForm from '../components/LoginForm';
+import FindingPassword from '../components/FindingPassword';
+
+export default {
+  name: 'LoginPage',
+  components: {
+    LoginForm,
+    FindingPassword,
+  },
+};
+</script>
+
+<style scoped>
+.login {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
