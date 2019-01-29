7be9c3177895ba9d16be9e36db0384e2c6b2acf3
yckim <yckim@crizen.com>
2019-01-29 20:43:06 +0900

    1. 이미지,비디오 읽기/쓰기 프로토타입 완료 커밋

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,10 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const index = require('./routes/index');
-const login = require('./routes/login');
+const member = require('./routes/member');
+const s3 = require('./routes/s3');
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +17,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/', login);
+app.use('/', member);
+app.use('/', s3);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -35,7 +33,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({body : err});
 });
 
 module.exports = app;
---
+++ backend/routes/s3.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+const downloader = require('s3-download-stream')
+const aws = require('aws-sdk')
+const awsConfig = require('../config/awsconfig');
+
+// config
+const auth = {
+    secretAccessKey: awsConfig.secretAccessKey,
+    accessKeyId: awsConfig.accessKeyId
+}
+
+var config = {
+    client: new aws.S3(auth),
+    concurrency: 6,
+    params: {
+        Bucket: awsConfig.bucket
+    }
+}
+
+router.get('/aws-image/profile/:fileName', (req,res,next) => {
+    config.params['Key'] = `profile/${req.params.fileName}`;
+    let img = downloader(config);
+    console.log('img : ',img);
+    img.pipe(res);
+});
+
+router.get('/aws-video/project/:fileName', (req,res,next) => {
+    config.params['Key'] = `project/${req.params.fileName}`;
+    let video = downloader(config);
+    console.log('video : ',video);
+    video.pipe(res);
+});
+
+module.exports = router;
+
+
