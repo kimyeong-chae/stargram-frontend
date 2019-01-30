5fa9b7ba6c83fce0298058f08dfe168c257f6dc9
mkki <gosorn1@gmail.com>
2019-01-30 20:08:20 +0900

    1. Toolbar 추가(기존 NavBar 대체)

---
+++ frontend/src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-toolbar flat dense>
+    <v-btn icon flat>
+      <v-icon @click.stop="$router.go(-1)">arrow_back</v-icon>
+    </v-btn>
+    <v-toolbar-title v-text="title"></v-toolbar-title>
+
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+};
+</script>
+
+<style scoped>
+.v-toolbar {
+  background-color: #ffffff;
+  border-bottom: 3px solid #f1f1f3;
+}
+.v-toolbar__title {
+  font-size: 15px;
+  font-weight: 500;
+  margin: 0;
+}
+</style>
