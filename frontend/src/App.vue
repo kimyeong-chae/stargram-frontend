4529e3d6e234de1a888e9ad4ceed3e5f70580965
mkki <gosorn1@gmail.com>
2019-01-31 21:10:02 +0900

    1. NavBar import 삭제

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,13 +6,11 @@
 </template>
 
 <script>
-import NavBar from './components/NavigationBar';
 import SideBar from './components/SideBar';
 
 export default {
   name: 'App',
   components: {
-    NavBar,
     SideBar,
   },
 };
