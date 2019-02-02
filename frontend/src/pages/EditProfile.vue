ecc1970530f7334922cae2e01772f9ba3afb82bf
mkki <gosorn1@gmail.com>
2019-02-03 01:15:17 +0900

    1. 프로필/프로필 수정 분기처리, 촬영 팝업 추가

--- frontend/src/pages/EditProfile.vue
+++ frontend/src/pages/EditProfile.vue
@@ -4,33 +4,46 @@
 
     <v-layout wrap row>
       <v-flex class="py-2" xs12 sm6>
-          <v-avatar extended size="90">
-            <v-img
-              v-if="member.profileUrl === ''"
-              :src="DefaultImagePath"
-            ></v-img>
+        <v-avatar extended size="90">
+          <v-img v-if="member.profileUrl === ''" :src="DefaultImagePath"></v-img>
 
-            <v-img v-else :src="member.profileUrl" dark></v-img>
+          <v-img v-else :src="member.profileUrl" dark></v-img>
 
+          <v-bottom-sheet v-model="bottomSheet">
             <v-btn
               id="select-picture"
               fab
               small
               color="#745bf5"
+              slot="activator"
               dark
               absolute
+              depressed
             >
               <v-icon>camera_alt</v-icon>
             </v-btn>
-          </v-avatar>
+
+            <v-list class="bottom-sheet">
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>사진 촬영</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+
+              <v-divider></v-divider>
+
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>앨범</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </v-list>
+          </v-bottom-sheet>
+        </v-avatar>
       </v-flex>
 
       <v-flex class="py-2" xs12 sm6>
-        <v-form
-          ref="form"
-          @submit.prevent="submit"
-          v-model="valid"
-        >
+        <v-form ref="form" @submit.prevent="submit" v-model="valid">
           <v-text-field
             v-model="nickname"
             :rules="nicknameRules"
@@ -48,7 +61,8 @@
             block
             depressed
             @click="submit"
-          >수정</v-btn>
+            v-text="selectText"
+          ></v-btn>
         </v-form>
       </v-flex>
     </v-layout>
@@ -67,6 +81,7 @@ export default {
     return {
       DefaultImagePath,
       valid: false,
+      bottomSheet: false,
       nickname: '',
       nicknameRules: [
         v => !!v || '닉네임을 입력해주세요.',
@@ -87,6 +102,11 @@ export default {
       this.$refs.form.validate();
     },
   },
+  computed: {
+    selectText() {
+      return this.menu.title === '프로필 수정' ? '수정' : '확인';
+    },
+  },
 };
 </script>
 
@@ -95,7 +115,11 @@ export default {
   padding: 31px;
 }
 #select-picture {
-  top:60px;
-  left:60px;
+  top: 60px;
+  left: 60px;
+}
+.bottom-sheet .v-list__tile__title {
+  font-size: 14px;
+  font-weight: 500;
 }
 </style>
