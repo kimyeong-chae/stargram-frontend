c66d62b5e986de84b1a8e349f4285194df293f6c
mkki <gosorn1@gmail.com>
2019-02-09 02:24:44 +0900

    material-desion-icon dependency 추가

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import Vue from 'vue';
 import 'babel-polyfill';
 import axios from 'axios';
