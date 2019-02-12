eacd16d96293509f8055d74e6f9d875f2a39dab6
mkki <gosorn1@gmail.com>
2019-02-13 03:21:58 +0900

    1. 툴바 타이틀 표시 분기문 추가

--- frontend/src/components/ToolBar.vue
+++ frontend/src/components/ToolBar.vue
@@ -3,7 +3,7 @@
     <v-btn v-if="title" icon flat :dark="color === 'transparent'">
       <v-icon @click.stop="$router.go(-1)">arrow_back</v-icon>
     </v-btn>
-    <v-toolbar-title v-if="title" v-text="title"></v-toolbar-title>
+    <v-toolbar-title v-if="color !== 'transparent' && title " v-text="title"></v-toolbar-title>
     <v-spacer></v-spacer>
     <v-toolbar-side-icon v-if="!title || color !== 'transparent'" dark @click.stop="toggleDrawer()"></v-toolbar-side-icon>
   </v-toolbar>
@@ -18,6 +18,9 @@ export default {
 <style scoped>
 .tool-bar-divider {
   border-bottom: 3px solid #f1f1f3 !important;
+}
+.white-text {
+  color: #ffffff;
 }
 .v-toolbar__title {
   font-size: 15px;
