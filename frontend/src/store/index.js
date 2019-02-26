3e6f619efeac15ea166d3beb1fa7bee13ad5e263
mkki <gosorn1@gmail.com>
2019-02-27 04:54:24 +0900

    sidebar 관련 속성 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,13 +5,17 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-
+    drawer: '',
   },
   mutations: {
-
+    toggleDrawer(state) {
+      state.drawer = !state.drawer;
+    },
   },
   actions: {
-
+    toggleDrawer({ commit }) {
+      commit('toggleDrawer');
+    },
   },
 });
 
