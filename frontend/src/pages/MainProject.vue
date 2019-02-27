a6e3f2c1cf3589f0437c901496a76d3d5ce905e8
mkki <gosorn1@gmail.com>
2019-02-28 06:09:22 +0900

    - 메인_내부 화면 추가

---
+++ frontend/src/pages/MainProject.vue
@@ -0,0 +1,486 @@
+<template>
+  <v-container fluid class="pa-0">
+    <v-layout row wrap>
+      <v-flex class="project-header" xs12>
+        <tool-bar title="project" color="transparent"></tool-bar>
+        <v-layout row wrap justify-center>
+          <v-flex class="py-2" xs10 sm10>
+            <div class="project-header-title text-xs-center">{{ user.project.title }}</div>
+          </v-flex>
+          <v-flex class="py-2" xs5 sm5>
+            <div class="project-header-time text-xs-center">
+              <span>{{ user.project.dueDate }}</span>
+            </div>
+          </v-flex>
+        </v-layout>
+      </v-flex>
+
+      <v-flex class="pa-0" xs12>
+        <v-container class="project-content" fluid grid-list-lg>
+          <v-layout row wrap>
+            <v-flex xs12>
+              <v-card flat class="project-comment">
+                <v-card-title class="project-card--px-1">
+                  <span class="project-card-title">인플루언스 코멘트</span>
+                </v-card-title>
+
+                <v-card-text
+                  class="project-card--px-1 project-card-text"
+                >A small River Named Duden Flows by their Place</v-card-text>
+
+                <v-card-actions class="pt-3">
+                  <v-layout>
+                    <v-flex shrink>
+                      <v-avatar color="grey lighten-2" size="30">
+                        <img :src="user.profileUrl">
+                      </v-avatar>
+                    </v-flex>
+                    <v-flex grow align-self-center>
+                      <span class="project-card-subtitle">{{ user.name }}</span>
+                    </v-flex>
+                    <v-flex shrink align-self-center>
+                      <span class="project-card-subtitle">35m</span>
+                    </v-flex>
+                  </v-layout>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-card flat class="project-intro">
+                <v-card-title class="project-card--px-1">
+                  <span class="project-card-title">프로젝트 소개</span>
+                </v-card-title>
+
+                <v-card-text
+                  class="project-card--px-1 pt-1 pb-3 project-card-text-2"
+                >{{ user.project.description }}</v-card-text>
+              </v-card>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-card flat class="project-progress">
+                <v-card-title class="project-card--px-1">
+                  <span class="project-card-title">모금 금액</span>
+                </v-card-title>
+
+                <v-card-text class="project-card--px-1 pt-1 project-card-text-3">
+                  <span class="project-card-text-3__1">{{ user.project.point | numberWithComma }}</span>
+                  <span class="project-card-text-3__2">/ {{ user.project.goal | numberWithComma }}</span>
+                  <span class="project-card-text-3__3">points</span>
+                </v-card-text>
+
+                <v-card-actions class="pt-1 pb-3">
+                  <v-progress-linear
+                    class="progress"
+                    :value="progressValue"
+                    color="#9a5af7"
+                    height="13"
+                    background-color="#f1f1f1"
+                  ></v-progress-linear>
+                </v-card-actions>
+              </v-card>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-card flat class="project-rank">
+                <div class="project-card--px-1 pb-2 bg-purple">
+                  <div class="project-card-title text-color--white">후원자 순위</div>
+
+                  <v-layout class="px-3 pt-3" text-xs-center>
+                    <v-flex class="pa-1" align-self-center>
+                      <div class="project-img--wrap">
+                        <span class="project-img--overlap">
+                          <img src="../assets/images/crown-bg-2.png">
+                        </span>
+                        <v-avatar class="project-avatar" size="68" color="grey lighten-2">
+                          <img :src="user.profileUrl" alt="alt">
+                        </v-avatar>
+                        <span class="project-badge project-badge--overlap bg-silver">500</span>
+                      </div>
+                      <div class="py-1 project-rank-subtitle">username</div>
+                    </v-flex>
+
+                    <v-flex class="pa-1" align-self-center shrink>
+                      <div class="project-img--wrap">
+                        <span class="project-img--overlap">
+                          <img src="../assets/images/crown-bg-1.png">
+                        </span>
+                        <v-avatar class="project-avatar" size="83" color="grey lighten-2">
+                          <img :src="user.profileUrl" alt="alt">
+                        </v-avatar>
+                        <span class="project-badge project-badge--overlap bg-gold">1000</span>
+                      </div>
+                      <div class="py-1 project-rank-subtitle">username</div>
+                    </v-flex>
+
+                    <v-flex class="pa-1" align-self-center>
+                      <div class="project-img--wrap">
+                        <span class="project-img--overlap">
+                          <img src="../assets/images/crown-bg-3.png">
+                        </span>
+                        <v-avatar class="project-avatar" size="68" color="grey lighten-2">
+                          <img :src="user.profileUrl" alt="alt">
+                        </v-avatar>
+                        <span class="project-badge project-badge--overlap bg-bronze">300</span>
+                      </div>
+                      <div class="py-1 project-rank-subtitle">username</div>
+                    </v-flex>
+                  </v-layout>
+                </div>
+
+                <div class="project-rank-list border-btm" v-for="i in 2" :key="i">
+                  <v-layout class="ma-0">
+                    <v-flex align-self-center shrink>
+                      <span class="project-rank-text__1">{{ i+3 }}</span>
+                    </v-flex>
+
+                    <v-flex align-self-center shrink>
+                      <v-avatar size="31" color="grey lighten-2">
+                        <img :src="user.profileUrl">
+                      </v-avatar>
+                    </v-flex>
+
+                    <v-flex align-self-center grow>
+                      <span class="project-rank-text__2">username</span>
+                    </v-flex>
+
+                    <v-flex align-self-center shrink>
+                      <span class="project-rank-text__3">point</span>
+                    </v-flex>
+                  </v-layout>
+                </div>
+
+                <div class="py-3 project-card-readmore" @click.stop="loadMore()">더보기</div>
+              </v-card>
+            </v-flex>
+
+            <v-flex xs12>
+              <v-card flat class="project-reply">
+                <v-card-title class="project-card--px-1">
+                  <span class="project-card-title">댓글</span>
+                </v-card-title>
+
+                <div
+                  class="project-reply-list border-btm"
+                  v-for="(reply, index) in user.project.replies"
+                  :key="index"
+                >
+                  <v-layout row wrap>
+                    <v-flex class="py-0" xs12>
+                      <v-layout class="ma-0">
+                        <v-flex class="pa-0" align-self-center shrink>
+                          <v-avatar size="31" color="grey lighten-2">
+                            <img :src="user.profileUrl">
+                          </v-avatar>
+                        </v-flex>
+
+                        <v-flex class="py-1" align-self-center grow>
+                          <div class="project-reply-text__1">username</div>
+                          <div class="project-reply-text__2">2019.01.01</div>
+                        </v-flex>
+
+                        <v-flex align-self-center shrink>
+                          <div v-if="reply.name === user.name" class="project-reply--btn">
+                            <span>owner</span>
+                          </div>
+                          <span v-if="reply.heart" class="project-reply--heart">
+                            <div class="project-img--wrap">
+                              <v-avatar size="28" color="grey lighten-2">
+                                <img :src="user.profileUrl" alt="alt">
+                              </v-avatar>
+                              <span class="project-img--overlap-2">
+                                <img src="../assets/images/heart-icon-bg-2.png">
+                              </span>
+                            </div>
+                          </span>
+                        </v-flex>
+                      </v-layout>
+                    </v-flex>
+
+                    <v-flex class="py-0" xs12>
+                      <v-layout class="ma-0">
+                        <v-flex class="pt-0 project-reply-list-bottom" align-self-center>
+                          <div class="project-reply-text__3">
+                            Years of old players have come to tell us
+                            something, and see what they have.
+                          </div>
+                        </v-flex>
+                      </v-layout>
+                    </v-flex>
+                  </v-layout>
+                </div>
+
+                <div class="py-3 project-card-readmore" @click.stop="loadMore()">더보기</div>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
+
+    <v-bottom-nav color="#f76592" :value="true" height="50" dark>
+      <v-btn dark>
+        <v-icon>mdi-cards-heart</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-container>
+</template>
+
+<script>
+export default {
+  components: {
+    ToolBar: () => import('../components/ToolBar'),
+  },
+  data() {
+    return {
+      user: {
+        name: 'Jack Jones',
+        profileUrl:
+          'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
+        project: {
+          title: 'Back to Nature Campaing Under The Star',
+          description:
+            'In the world of business, the law of two-thirds offers criteria for decisions about what will define a product or organization.',
+          dueDate: '2d:14h:35m:34s',
+          point: 1203,
+          goal: 2000,
+          participant: 128,
+          replies: [
+            { name: 'Jack Jones', heart: '' },
+            {
+              name: 'test',
+              heart: [
+                {
+                  name: 'Jack Jones',
+                  profileUrl:
+                    'https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light',
+                },
+              ],
+            },
+            { name: 'test2', heart: '' },
+          ],
+        },
+      },
+    };
+  },
+  computed: {
+    progressValue() {
+      const progressValue =
+        (this.user.project.point / this.user.project.goal) * 100;
+      return progressValue;
+    },
+  },
+  filters: {
+    numberWithComma(value) {
+      return value
+        .toString()
+        .split('')
+        .reverse()
+        .reduce((acc, digit, i) => {
+          if (i > 0 && i % 3 === 0) acc.push(',');
+          return [...acc, digit];
+        }, [])
+        .reverse()
+        .join('');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.v-card {
+  border-radius: 15px;
+}
+.project-header {
+  background: url("../assets/images/main-image.jpeg");
+  background-size: 100%;
+  height: 250px;
+}
+.project-header-title {
+  font-size: 29px;
+  font-weight: 600;
+  line-height: 1.15;
+  color: white;
+}
+.project-header-time {
+  border-radius: 12px;
+  background: rgba(154, 90, 247, 0.7);
+}
+.project-header-time > span {
+  font-size: 15.3px;
+  font-weight: 600;
+  color: white;
+}
+.project-content {
+  margin-top: -30px;
+  padding: 10px;
+  border-top-left-radius: 15px;
+  border-top-right-radius: 15px;
+  background-color: #f5f5f8;
+}
+.project-comment {
+  background: linear-gradient(281deg, #8e59ff, #b58bff);
+  font-family: NotoSansKR;
+  color: white;
+}
+.project-card-title {
+  font-size: 13px;
+  font-weight: bold;
+}
+.project-card-text {
+  font-size: 18px;
+  font-weight: 900;
+  line-height: 1.31;
+  letter-spacing: -0.2px;
+}
+.project-card-text-2 {
+  font-size: 12.3px;
+  font-weight: normal;
+  line-height: 1.47;
+  letter-spacing: -0.3px;
+  color: #9a5af7;
+}
+.project-card-subtitle {
+  font-size: 12px;
+  font-weight: bold;
+}
+.project-card--px-1 {
+  padding: 10px 10px 0px 10px;
+}
+.project-card--mx-1 {
+  margin: 0px 10px;
+}
+.project-progress .v-progress-linear {
+  border-radius: 18px;
+}
+.project-card-text-3 {
+  font-family: NotoSans;
+  color: #9a5af7;
+}
+.project-card-text-3__1 {
+  font-size: 28.8px;
+  font-weight: 900;
+}
+.project-card-text-3__2 {
+  font-size: 14.3px;
+  font-weight: bold;
+}
+.project-card-text-3__3 {
+  font-size: 10.8px;
+  font-weight: 500;
+}
+.bg-purple {
+  background: linear-gradient(281deg, #8e59ff, #b58bff);
+}
+.bg-gold {
+  background-color: #ffc842;
+}
+.bg-silver {
+  background-color: #d1dadf;
+}
+.bg-bronze {
+  background-color: #d0bba9;
+}
+.border-btm {
+  border-bottom: 1px solid #e8e8e8;
+}
+.text-color--white {
+  color: white;
+}
+.project-card-readmore {
+  font-size: 14px;
+  font-weight: normal;
+  color: #9a5af7;
+  cursor: pointer;
+  text-align: center;
+}
+.project-avatar > img {
+  border: 4px solid #ffffff;
+}
+.project-img--wrap {
+  position: relative;
+}
+.project-img--overlap {
+  position: absolute;
+  top: -15px;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+.project-badge {
+  width: 45px;
+  height: 20px;
+  border-radius: 10px;
+  color: #ffffff;
+}
+.project-badge--overlap {
+  position: absolute;
+  top: auto;
+  bottom: -3px;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
+.project-rank-subtitle {
+  font-size: 13px;
+  font-weight: 500;
+}
+.project-rank-list {
+  padding: 10px 0;
+  margin: 0 10px;
+}
+.project-rank-text__1 {
+  font-size: 14px;
+  color: #9d9d9d;
+}
+.project-rank-text__2 {
+  font-size: 13px;
+  color: #333333;
+}
+.project-rank-text__3 {
+  font-size: 15px;
+  color: #333333;
+}
+.project-reply-list {
+  padding: 10px 0;
+  margin: 0 10px;
+}
+.project-reply--btn {
+  width: 53px;
+  height: 17px;
+  border-radius: 6px;
+  background-color: #a168f8;
+  font-size: 12.3px;
+  font-weight: 500;
+  color: #ffffff;
+  text-align: center;
+  font-family: NotoSansKR;
+}
+.project-reply-list-bottom {
+  margin-left: 31px;
+}
+.project-reply-text__1 {
+  font-size: 13px;
+  font-weight: 500;
+  color: #333333;
+}
+.project-reply-text__2 {
+  font-size: 11px;
+  font-weight: 500;
+  color: #b0b0b0;
+}
+.project-reply-text__3 {
+  font-family: NotoSansKR;
+  font-size: 12px;
+  line-height: 1.25;
+  color: #444444;
+}
+.project-img--overlap-2 {
+  position: absolute;
+  top: auto;
+  bottom: -6px;
+  left: auto;
+  right: -3px;
+}
+</style>
