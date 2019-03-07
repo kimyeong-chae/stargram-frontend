1fcc8a6b4e79304ca07c66ba16972cea06687014
mkki <gosorn1@gmail.com>
2019-03-08 06:55:56 +0900

    - fetch member 기능 추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,20 +1,33 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import FamenceAPI from '../api/famenceAPI';
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
     drawer: false,
+    member: {},
   },
   mutations: {
     toggleDrawer(state) {
       state.drawer = !state.drawer;
     },
+    fetchMemberOne(state, payload) {
+      state.member = payload;
+    },
   },
   actions: {
     toggleDrawer({ commit }) {
       commit('toggleDrawer');
+    },
+    async fetchMemberOne({ commit }, memberId) {
+      try {
+        const response = await FamenceAPI.fetchMemberOne(memberId);
+        commit('fetchMemberOne', response.data);
+      } catch (error) {
+        // implementation error handling
+      }
     },
   },
 });
